06562fc7adf0a57fe6e5080568fdfdba4e5c2a6c
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-01-17 14:55:43 +0530

    handling event

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "🙄 Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+*/
+
+// handing click event
+
+const x = function () {
+  console.log(23);
+};
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ no Number!";
+  }
+});
